bc1d1a295de1cb3fb1dc32b630179308640afa86
gonzaloferreyra2020 <86134463+gonzaloferreyra2020@users.noreply.github.com>
2022-03-07 13:29:23 -0300

    Add files via upload

---
+++ js/productos.js
@@ -0,0 +1,44 @@
+[
+    {
+        "precio": 35800,
+        "id": 1,
+        "title": "Bicicleta",
+        "thumbnailUrl": "https://image.shutterstock.com/image-photo/yellow-black-29er-mountainbike-thick-600w-1498702814.jpg"
+    },
+    {
+        "precio": 89500,
+        "id": 2,
+        "title": "Notebook",
+        "thumbnailUrl": "https://image.shutterstock.com/image-vector/laptop-flat-opened-computer-screen-600w-1911448006.jpg"
+    },
+    {
+        "precio": 22600,
+        "id": 3,
+        "title": "Auriculares",
+        "thumbnailUrl": "https://image.shutterstock.com/image-vector/headphones-icon-600w-159940955.jpg"
+    },
+    {
+        "precio": 8500,
+        "id": 4,
+        "title": "Zapatillas",
+        "thumbnailUrl": "https://image.shutterstock.com/image-photo/vintage-red-shoes-on-white-600w-92008067.jpg"
+    },
+    {
+        "precio": 15450,
+        "id": 5,
+        "title": "Cafetera",
+        "thumbnailUrl": "https://image.shutterstock.com/image-photo/auto-drip-coffee-maker-isolated-600w-1918349963.jpg"
+    },
+    {
+        "precio": 56900,
+        "id": 6,
+        "title": "Ropero",
+        "thumbnailUrl": "https://image.shutterstock.com/image-illustration/dark-brown-wardrobe-open-doors-600w-592920866.jpg"
+    },
+    {
+        "precio": 189000,
+        "id": 7,
+        "title": "Televisor",
+        "thumbnailUrl": "https://image.shutterstock.com/image-photo/4k-monitor-isolated-on-white-600w-357968483.jpg"
+    }
+]
